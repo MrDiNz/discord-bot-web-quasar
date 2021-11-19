2726757f9ef2256035b5d7fd447a0d1ce44f1154
MrDiNz <poo.hu@hotmail.com>
2021-11-19 23:39:02 +0700

    init + temp text area

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page>
+    <div class="row justify-center">
+      <q-input outlined type="textarea" v-model="text" class="col-12" />
+      <q-btn class="col-auto" color="primary" @click="postToDiscord">
+        post
+      </q-btn>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {},
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    async postToDiscord() {
+      await this.$axios.post(process.env.webhook_url as string, {
+        content: this.text
+      })
+    }
+  }
+})
+</script>
